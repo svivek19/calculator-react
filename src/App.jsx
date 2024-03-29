76a27d0ac900625c0cf3995e6fa46b5e75d6e7fc
svivek19 <svivek030503@gmail.com>
2024-03-29 11:05:14 +0530

    style finish

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
     try {
       setInput(eval(input).toString());
     } catch (err) {
-      setInput("Invaild");
+      setInput("Error");
     }
   };
 
@@ -27,19 +27,31 @@ const App = () => {
         <button onClick={() => handleBtn("1")}>1</button>
         <button onClick={() => handleBtn("2")}>2</button>
         <button onClick={() => handleBtn("3")}>3</button>
-        <button onClick={() => handleBtn("+")}>+</button>
+        <button className="opt" onClick={() => handleBtn("+")}>
+          +
+        </button>
         <button onClick={() => handleBtn("4")}>4</button>
         <button onClick={() => handleBtn("5")}>5</button>
         <button onClick={() => handleBtn("6")}>6</button>
-        <button onClick={() => handleBtn("-")}>-</button>
+        <button className="opt" onClick={() => handleBtn("-")}>
+          -
+        </button>
         <button onClick={() => handleBtn("7")}>7</button>
         <button onClick={() => handleBtn("8")}>8</button>
         <button onClick={() => handleBtn("9")}>9</button>
-        <button onClick={() => handleBtn("*")}>*</button>
+        <button className="opt" onClick={() => handleBtn("*")}>
+          *
+        </button>
         <button onClick={() => handleBtn(".")}>.</button>
         <button onClick={() => handleBtn("0")}>0</button>
-        <button onClick={() => handleBtn("/")}>/</button>
-        <button onClick={clear}>C</button>
+        <button className="opt" onClick={() => handleBtn("/")}>
+          /
+        </button>
+        <button className="clear" onClick={clear}>
+          C
+        </button>
+      </div>
+      <div className="equal-div">
         <button onClick={equal}>=</button>
       </div>
     </div>
