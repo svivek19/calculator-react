3703c559c0f895cdb2c2225f6a9cc076cd537630
svivek19 <svivek030503@gmail.com>
2024-03-29 10:31:13 +0530

    logic finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
-function App() {
+const App = () => {
+  const [input, setInput] = useState("");
+
+  const handleBtn = (val) => {
+    setInput((prevInput) => prevInput + val);
+  };
+
+  const clear = () => {
+    setInput("");
+  };
+
+  const equal = () => {
+    try {
+      setInput(eval(input).toString());
+    } catch (err) {
+      setInput("Invaild");
+    }
+  };
+
   return (
     <div className="calculator">
-      <input type="text" readOnly />
+      <input type="text" value={input} readOnly />
       <div className="buttons">
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>+</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>-</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>*</button>
-        <button>0</button>
-        <button>.</button>
-        <button>/</button>
-        <button>C</button>
-        <button>=</button>
+        <button onClick={() => handleBtn("1")}>1</button>
+        <button onClick={() => handleBtn("2")}>2</button>
+        <button onClick={() => handleBtn("3")}>3</button>
+        <button onClick={() => handleBtn("+")}>+</button>
+        <button onClick={() => handleBtn("4")}>4</button>
+        <button onClick={() => handleBtn("5")}>5</button>
+        <button onClick={() => handleBtn("6")}>6</button>
+        <button onClick={() => handleBtn("-")}>-</button>
+        <button onClick={() => handleBtn("7")}>7</button>
+        <button onClick={() => handleBtn("8")}>8</button>
+        <button onClick={() => handleBtn("9")}>9</button>
+        <button onClick={() => handleBtn("*")}>*</button>
+        <button onClick={() => handleBtn(".")}>.</button>
+        <button onClick={() => handleBtn("0")}>0</button>
+        <button onClick={() => handleBtn("/")}>/</button>
+        <button onClick={clear}>C</button>
+        <button onClick={equal}>=</button>
       </div>
     </div>
   );
-}
+};
 
 export default App;
